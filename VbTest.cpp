a19b58da45f4a509aef87789bd89a6449313302a
Roulandu <497896801@qq.com>
2018-03-21 15:48:37 +0800

    test
    shishi

---
+++ VbTest.cpp
@@ -0,0 +1,21 @@
+#include "snmp_pp.h"
+#include <iostream.h>
+#include "Oid.h"
+
+void main(){
+	Oid oid;
+	oid = "1. 3. 6. 1. 2. 1";
+	Vb vb((Oid)"1. 2. 3. 4. 5. 6");
+	cout<<"Vb's oid is: "<<vb.get_printable_oid()<<endl;
+	vb.set_oid(oid);
+	cout<<"Vb's oid is: "<<vb.get_printable_oid()<<endl;
+	cout<<"Vb's value is: "<<vb.get_printable_value()<<endl;
+	int i=1000;
+	vb.set_value(i);
+	cout<<"Vb's value is: "<<vb.get_printable_value()<<endl;
+	int value;
+	vb.get_value(value);
+	cout<<"Vb's value is: "<<value<<endl;
+	vb.set_null();
+	cout<<"Vb's value is: "<<vb.get_printable_value()<<endl;
+}
